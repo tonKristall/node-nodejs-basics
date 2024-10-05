a8b55107b40326d194da2a75c07543c42dd7d51a
Aleksander Shustov <krisale@yandex.ru>
2024-10-05 22:00:43 +0300

    feat: add command line interface

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const envVariables = process.env;
+  const result = [];
+
+  for (const [key, value] of Object.entries(envVariables)) {
+    if (key.startsWith(prefix)) {
+      result.push(`${key}=${value}`);
+    }
+  }
+
+  if (result.length) {
+    console.log(result.join('; '));
+  }
 };
 
 parseEnv();
