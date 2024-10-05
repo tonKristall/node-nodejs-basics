7776f9ccb5e7bd4a94d6b98db552d8fb2e4719ce
Aleksander Shustov <krisale@yandex.ru>
2024-10-05 23:25:21 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const directoryName = 'files';
+  const fileName = 'fileToCalculateHashFor.txt';
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const directoryPath = path.join(__dirname, directoryName);
+  const filePath = path.join(directoryPath, fileName);
+  const stream = createReadStream(filePath);
+  const hash = createHash('sha256');
+
+  stream.on('data', chunk => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    const hexHash = hash.digest('hex');
+    console.log(hexHash);
+  });
 };
 
 await calculateHash();
