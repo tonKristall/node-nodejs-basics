590c0d556d3cf192032e821ea0cf21843a13a0d9
Aleksander Shustov <krisale@yandex.ru>
2024-10-06 18:29:44 +0300

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const dirName = 'files';
+  const fileName = 'fileToRead.txt';
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(__dirname, dirName, fileName);
+  const readableStream = createReadStream(filePath);
+
+  readableStream.pipe(process.stdout);
 };
 
 await read();
